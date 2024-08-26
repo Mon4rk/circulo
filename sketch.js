07347b034b04b0781391cf7a8dff34e5c04d469b
alisson <alisson.campos.almeida@escola.pr.gov.br>
2024-08-26 12:15:55 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800, 800);
+    background('black');
+  }
+  
+  function draw() {
+    stroke ('white');
+    fill ('gray')
+    
+    
+    if (mouseIsPressed) {
+      rect (mouseX, mouseY, 20, 35)
+    }
+  }
